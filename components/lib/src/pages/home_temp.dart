91870489b940d8fe01118918ad3664655593c4d1
Lucas Heim <lucasedfc@gmail.com>
2020-03-01 19:47:21 -0300

    List method 2

--- components/lib/src/pages/home_temp.dart
+++ components/lib/src/pages/home_temp.dart
@@ -9,28 +9,43 @@ class HomePageTemp extends StatelessWidget {
       appBar: AppBar(
         title: Text('Componentes Temp'),
       ),
-      body: ListView(children: _createItems()),
+      body: ListView(children: _createItemsShort()),
     );
   }
 
   List<Widget> _createItems() {
-
     List<Widget> list = new List<Widget>();
 
     for (String opt in options) {
-
       final tempWidget = ListTile(
         title: Text(opt),
       );
 
-      list..add(tempWidget)
-          ..add(Divider(color: Colors.blue,
-      ));
+      list
+        ..add(tempWidget)
+        ..add(Divider(
+          color: Colors.blue,
+        ));
     }
     return list;
   }
 
-  List<Widget> createItemsShort() {
-    
+  List<Widget> _createItemsShort() {
+    return options.map((item) {
+
+      return Column(
+        children: <Widget>[
+          ListTile(
+            subtitle: Text('Subtitle'),
+            leading: Icon(Icons.ac_unit),
+            trailing: Icon(Icons.arrow_right),
+            title: Text(item + '!'),
+            onTap: (){},
+          ),
+          Divider()
+        ],
+      );
+    }).toList();
+
   }
 }
