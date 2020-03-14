d52d0803d0cf2e0f272961cfda77a844caa4f112
Lucas Heim <lucasedfc@gmail.com>
2020-03-14 14:24:13 -0300

    Card

---
+++ components/lib/src/pages/card_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class CardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cards'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[_cardType1()],
+      ),
+    );
+  }
+}
+
+Widget _cardType1() {
+  return Card(
+    child: Column(
+      children: <Widget>[
+        ListTile(
+          leading: Icon(Icons.photo_album, color: Colors.blue),
+          title: Text('Card Title'),
+          subtitle: Text('This is a description for the flutter card'),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            FlatButton(
+              child: Text('Cancel'),
+              onPressed: () {},
+            )
+          ],
+        )
+      ],
+    ),
+  );
+}
--- components/lib/src/routes/routes.dart
+++ components/lib/src/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:components/src/pages/alert_page.dart';
 import 'package:components/src/pages/avatar_page.dart';
+import 'package:components/src/pages/card_page.dart';
 import 'package:components/src/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
@@ -9,5 +10,6 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
         '/': (BuildContext context) => HomePage(),
         'alert': (BuildContext context) => AlertPage(),
         'avatar': (BuildContext context) => AvatarPage(),
+        'card': (BuildContext context) => CardPage(),
       };
 }
