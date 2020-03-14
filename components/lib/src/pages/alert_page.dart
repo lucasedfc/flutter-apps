1e34f29b83d4a74d204f2422553995f33423b514
Lucas Heim <lucasedfc@gmail.com>
2020-03-14 15:11:11 -0300

    Modals

--- components/lib/src/pages/alert_page.dart
+++ components/lib/src/pages/alert_page.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
 class AlertPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Alert Page')
+      appBar: AppBar(title: Text('Alert Page')),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Show Alert'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => _showAlert(context),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add_location),
@@ -15,5 +21,34 @@ class AlertPage extends StatelessWidget {
         },
       ),
     );
+  }
+
+  void _showAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Title'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[Text('Content'),
+              FlutterLogo(size: 100)
+              ],
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Cancel'),
+                onPressed: () => Navigator.of(context).pop(),
+                ),
+                FlatButton(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                )
+            ],
+          );
+        });
   }
 }
