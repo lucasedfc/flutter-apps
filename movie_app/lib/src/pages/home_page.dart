0613ad829db527448f76c0bf22c7a6269f33e4f7
Lucas Heim <lucasedfc@gmail.com>
2020-03-21 16:00:08 -0300

    Card swiper

--- movie_app/lib/src/pages/home_page.dart
+++ movie_app/lib/src/pages/home_page.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/src/widgets/card_swiper_widget.dart';
+
+
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('Hello!')),
+      appBar: AppBar(
+        title: Text('Movies on Cinema'),
+        backgroundColor: Colors.indigoAccent,
+        centerTitle: false,
+        actions: <Widget>[
+          IconButton(
+          icon: Icon(Icons.search),
+          onPressed: () {}
+          )
+        ],
+      ),
+      body: Container(
+        child: Column(children: <Widget>[
+          _swiperCard()
+        ],),
+      ),
     );
+  }
+
+  Widget _swiperCard() {
+     return CardSwiper(
+      movies: [1,2,3,4,5],
+    ); 
+    
   }
 }
