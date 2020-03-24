97a8558c780ee64cc9dc4d08827557deaf534106
Lucas Heim <lucasedfc@gmail.com>
2020-03-24 13:07:17 -0300

    Inir project design

---
+++ design/lib/main.dart
@@ -0,0 +1,14 @@
+// TODO Implement this library.
+import 'package:flutter/material.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Design'
+    );
+  }
+}
