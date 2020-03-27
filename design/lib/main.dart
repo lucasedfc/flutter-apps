e40c5152bd983eb191868003bbcfaeb2a6707052
Lucas Heim <lucasedfc@gmail.com>
2020-03-27 16:19:33 -0300

    Buttons part 2

--- design/lib/main.dart
+++ design/lib/main.dart
@@ -8,6 +8,13 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    /* SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle.light.copyWith(
+        statusBarColor: Colors.white
+      )
+    ); */
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Design',
--- design/lib/src/pages/buttons_page.dart
+++ design/lib/src/pages/buttons_page.dart
@@ -8,10 +8,114 @@ class ButtonsPage extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: <Widget>[
-          _backgroupApp()
+          _backgroupApp(),
+          SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                _titles(),
+                _buttons()
+              ],
+            ),
+          )
         ],
-      )
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        fixedColor: Colors.pinkAccent,
+        backgroundColor: Color.fromRGBO(55, 57, 84, 1.0),
+        unselectedItemColor: Color.fromRGBO(116, 117, 152, 1.0),
+        items: <BottomNavigationBarItem> [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today, size: 30.0),
+            title: Container()
+            ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.pie_chart_outlined, size: 30.0),
+            title: Container()
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.supervised_user_circle, size: 30.0),
+            title: Container()
+          )
+        ],
+      ),
       
+    );
+  }
+
+  Widget _titles() {
+    return SafeArea(
+            child: Container(
+            padding: EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text('Classify transaction', 
+              style: TextStyle(color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.bold)),
+              SizedBox(height: 10.0),
+              Text('Classify this transaction in a particular category',
+              style: TextStyle(color: Colors.white, fontSize: 18.0))
+            ],
+          ),
+      ),
+    );
+  }
+
+  Widget _buttons() {
+    return Table(
+      children: [
+        TableRow(
+        children: [
+          _createButton(),
+          _createButton()
+        ]
+      ),
+      TableRow(
+        children: [
+          _createButton(),
+          _createButton()
+        ]
+      ),
+      TableRow(
+        children: [
+          _createButton(),
+          _createButton()
+        ]
+      ),
+      TableRow(
+        children: [
+          _createButton(),
+          _createButton()
+        ]
+      )
+      ]);
+  }
+
+  Widget _createButton() {
+    return Container(
+      height: 180.0,
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30.0),
+        color: Color.fromRGBO(62, 66, 107, 0.7)
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          SizedBox(
+            height: 5.0,
+          ),
+          CircleAvatar(
+            backgroundColor: Colors.pinkAccent,
+            radius: 35.0,
+            child: Icon(Icons.supervised_user_circle, color: Colors.white, size: 30.0),
+          ),
+          Text('Item', style: TextStyle(color: Colors.pinkAccent)),
+          SizedBox(
+            height: 5.0,
+          )
+
+        ],
+      ),
     );
   }
 }
