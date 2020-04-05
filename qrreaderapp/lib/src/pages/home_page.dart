861573091238b386afcb1518de248d3020ea38ad
Lucas Heim <lucasedfc@gmail.com>
2020-04-05 19:58:36 -0300

    Navigation bottom bar

--- qrreaderapp/lib/src/pages/home_page.dart
+++ qrreaderapp/lib/src/pages/home_page.dart
@@ -1,14 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:qrreaderapp/src/pages/addresses_page.dart';
+import 'package:qrreaderapp/src/pages/maps_page.dart';
 
-class HomePage extends StatelessWidget {
-  
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home Page'),
-      ),
+      body: _loadPage(currentIndex),
+      bottomNavigationBar: _createBottomNavigationBar(),
     );
+  }
+
+  Widget _createBottomNavigationBar() {
+    return BottomNavigationBar(
+      currentIndex: currentIndex, 
+      onTap: (index) {
+        setState(() {
+        currentIndex = index;
+        });
+      }, 
+      items: [
+      BottomNavigationBarItem(icon: Icon(Icons.map), title: Text('Maps')),
+      BottomNavigationBarItem(icon: Icon(Icons.brightness_5), title: Text('Address'))
+    ]);
+  }
+
+  Widget _loadPage(int actualPage) {
+
+    switch (actualPage) {
+      case 0:
+        return MapsPage();
+        break;
+      case 1:
+        return AddressesPage();
+        break;
+      default:
+        return MapsPage();
+    }
   }
 }
