1dbd2efb9aced74b8ded092af28c2246f5202cac
Lucas Heim <lucasedfc@gmail.com>
2020-04-07 19:20:29 -0300

    init sqflite

---
+++ qrreaderapp/lib/src/providers/db_provider.dart
@@ -0,0 +1,46 @@
+
+
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DBProvider {
+
+// singleton - one db instance
+  static Database _database;
+  static final DBProvider db = DBProvider._(); //Private constructor
+
+  DBProvider._();
+
+  Future<Database> get database async {
+
+    if( _database != null) return _database;
+
+    _database = await initDB();
+
+    return _database;
+  }
+
+  initDB() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+
+    final path = join( documentsDirectory.path, 'ScansDB.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onOpen: (db) {},
+      onCreate: (Database db, int version) async {
+        await db.execute(
+          'CREATE TABLE Scans ('
+          'id INTEGER PRIMARY KEY,'
+          'type TEXT,'
+          'value TEXT'
+          ')'
+        );
+      }
+      );
+  }
+
+}
