5b04dcbee9cf4f9f0ab6381b1e24efcf9b11f9ce
Lucas Heim <lucasedfc@gmail.com>
2020-04-09 17:32:18 -0300

    map type

--- qrreaderapp/lib/src/pages/open_map_page.dart
+++ qrreaderapp/lib/src/pages/open_map_page.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:qrreaderapp/src/models/scan_model.dart';
-import 'package:latlong/latlong.dart';
 
-class MapPage extends StatelessWidget {
+
+class MapPage extends StatefulWidget {
+
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  MapController mapController = new MapController();
+
+  String mapType = 'streets';
+
   @override
   Widget build(BuildContext context) {
     final ScanModel scan = ModalRoute.of(context).settings.arguments;
@@ -11,14 +21,22 @@ class MapPage extends StatelessWidget {
         appBar: AppBar(
           title: Text('Coordinates QR'),
           actions: <Widget>[
-            IconButton(icon: Icon(Icons.my_location), onPressed: () {})
+            IconButton(icon: Icon(Icons.my_location), 
+            onPressed: () {
+              mapController.move(scan.getLatLng(), 15);
+            }
+            )
           ],
         ),
-        body: _createFlutterMap(scan));
+        body: _createFlutterMap(scan),
+        floatingActionButton: _createFloatingButton(context),
+        );
+        
   }
 
   Widget _createFlutterMap(ScanModel scan) {
     return FlutterMap(
+      mapController: mapController,
       options: MapOptions(center: scan.getLatLng(), zoom: 15),
       layers: [
         _createMap(),
@@ -33,7 +51,7 @@ class MapPage extends StatelessWidget {
       '{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}',
       additionalOptions: {
         'accessToken': 'pk.eyJ1IjoibHVjYXNlZGZjIiwiYSI6ImNrOHQ1amlwdzAyMTgzZ281NHNkMmZic2EifQ.eUAu7igZRLbPUAfw-NNYSw',
-        'id': 'mapbox.streets' // streets, dark, light, sattelite, outdoors
+        'id': 'mapbox.$mapType' // streets, dark, light, sattelite, outdoors
       }
     );
   }
@@ -49,5 +67,28 @@ class MapPage extends StatelessWidget {
          )
        ]
      );
+  }
+
+  Widget _createFloatingButton(BuildContext context) {
+    return FloatingActionButton(
+      child: Icon(Icons.repeat),
+      backgroundColor: Theme.of(context).primaryColor,
+      onPressed: () {
+
+        if(mapType == 'streets') {
+          mapType = 'dark';
+        } else if (mapType == 'dark') {
+          mapType = 'light';
+        } else if (mapType == 'light') {
+          mapType = 'outdoors';
+        } else if (mapType == 'outdoors') {
+          mapType = 'sattelite';
+        } else {
+          mapType = 'streets';
+        }
+        setState(() {});
+
+      }
+      );
   }
 }
